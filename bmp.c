b14365f41f52b96d0731b3b5ec268da315896744
lisbon <16432726+honeydatax@users.noreply.github.com>
2022-02-06 18:11:21 +0000

    Add files via upload

--- bmp.c
+++ bmp.c
@@ -41,7 +41,7 @@ void setBMPHead(FILE *f1,int w,int h){
 	//bmp -------
 //	hb->ID[0]='B';
 //	hb->ID[1]='M';
-	hb.size=54+(w*3+2)*h;
+	hb.size=54+(w*3)*h;
 	hb.application=0;
 	hb.pos=54;
 	//dib -------
@@ -50,10 +50,10 @@ void setBMPHead(FILE *f1,int w,int h){
 	hb.h=h;
 	hb.nPlanes1=1;
 	hb.nPlanes2=0;
-	hb.nPixel1=16;
+	hb.nPixel1=24;
 	hb.nPixel2=0;
 	hb.compressions=0;
-	hb.rawSize=(w*3+2)*h;
+	hb.rawSize=(w*3)*h;
 	hb.rw=2835;
 	hb.rh=2835;
 	hb.colors=0;
@@ -66,20 +66,28 @@ void setBMPHead(FILE *f1,int w,int h){
 }
 int main(int argc,char *argv[]){
 	char *files="my.bmp";
+	struct lines ln;
 	struct BM bm;
 	FILE *f1;
 	int i;
 	int ii;
 	printf("\ec\e[44;36m\n\a\n");
 	f1=fopen(files,"w");
+	
 		if(f1!=NULL){
 			setBMPHead(f1,64,64);
+			printf("%d\n",(int)ftell(f1));
 			for(i=0;i<64;i++){
 				for(ii=0;ii<64;ii++){
 						bm.liness[i].px[ii].b=255;
-						bm.liness[i].px[ii].g=i*4;
-						bm.liness[i].px[ii].r=i*4;
+						bm.liness[i].px[ii].g=0;
+						bm.liness[i].px[ii].r=0;
 				}
+			}
+			for(i=0;i<64;i++){
+				bm.liness[4].px[i].b=0;
+				bm.liness[4].px[i].g=0;
+				bm.liness[4].px[i].r=0;
 			}
 			fwrite(&bm,sizeof(bm),1,f1);
 			fclose(f1);
